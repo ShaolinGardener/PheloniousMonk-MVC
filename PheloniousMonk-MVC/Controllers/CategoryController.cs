c1215f05b4db4805d7b6a01452df3cf056da9389
zoso2 <zoso2@DESKTOP-7ESEBEU>
2021-11-02 21:40:08 -0400

    Add project files.

---
+++ PheloniousMonk-MVC/Controllers/CategoryController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PheloniousMonk_MVC.Data;
+using PheloniousMonk_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PheloniousMonk_MVC.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+
+        // Get for create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(cat);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cat);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            var cat = _db.Category.Find(id);
+            if (cat == null)
+                return NotFound();
+
+            return View(cat);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category cat)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(cat);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(cat);
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            var cat = _db.Category.Find(id);
+            if (cat == null)
+                return NotFound();
+
+            return View(cat);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            var cat = _db.Category.Find(id);
+            if (cat == null)
+                return NotFound();
+            _db.Category.Remove(cat);
+            _db.SaveChanges();
+            return View(cat);
+        }
+
+    }
+}
---
+++ PheloniousMonk-MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using PheloniousMonk_MVC.Models;
+
+namespace PheloniousMonk_MVC.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+               
+        }
+
+        public DbSet<Category> Category { get; set; }
+      
+    }
+}
